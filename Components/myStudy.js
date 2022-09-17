aea926734882eb3453a8ada6b54d04cae9e39ae8
Rafiul-Islam <76603094+Abdur-Rafiul@users.noreply.github.com>
2022-09-17 20:16:21 +0600

    r

--- Components/myStudy.js
+++ Components/myStudy.js
@@ -16,20 +16,20 @@ class MyStudy extends Component {
     }
     render() {
         return (
-            <Container fluid={true} className="text-center educationBackground">
+            <Container fluid={true} className="educationBackground">
                 <Container>
                 <Row>
-                    <h2 className="studyMainTitle">Education</h2>
+                    <h2 className="studyMainTitle text-center">Education</h2>
                     <Col lg={4} md={6} sm={12}>
 
                         <Card className="studyCard"  data-aos="fade-up"
                               data-aos-offset="200">
-                            <h3 className="title">Madrasha</h3>
+                            <h3 className="title text-center">Madrasha</h3>
                             <Card.Img width="100%" variant="top" src="/Photo/madrash.jpg" />
                             <Card.Body>
                                 <Card.Title><span className="studyTitle">Miapur Dakhil Madrasha</span></Card.Title>
                                 <Card.Text>
-                                    <span className="studyDescription">
+                                    <span className="studyDescription text-justify">
                                     I feel blessed to have studied at Miyapur Dakhil Madrasa. When I think of Madrasa, I remember that childhood.
                                     </span>
                                 </Card.Text>
@@ -41,12 +41,12 @@ class MyStudy extends Component {
 
                     <Col lg={4} md={6} sm={12}>
                         <Card className="studyCard" data-aos="fade-down">
-                            <h3 className="title">HSC</h3>
+                            <h3 className="title text-center">HSC</h3>
                             <Card.Img width="100%" variant="top" src="/Photo/college.jpg" />
                             <Card.Body>
                                 <Card.Title><span className="studyTitle">Miapur Haji Jasimuddin High School & College</span></Card.Title>
                                 <Card.Text>
-                                    <span className="studyDescription">
+                                    <span className="studyDescription text-justify">
                                     I feel blessed to have studied at Miapur Haji Jasimuddin High School & College. When I think of Madrasa, I remember that childhood.
                                     </span>
                                 </Card.Text>
@@ -57,12 +57,12 @@ class MyStudy extends Component {
 
                     <Col lg={4} md={6} sm={12}>
                         <Card className="studyCard" data-aos="fade-up">
-                            <h3 className="title">University</h3>
+                            <h3 className="title text-center">University</h3>
                             <Card.Img width="100%" variant="top" src="/Photo/daffodil.jpg" />
                             <Card.Body>
                                 <Card.Title><span className="studyTitle">Daffodil International University</span></Card.Title>
                                 <Card.Text>
-                                    <span className="studyDescription">
+                                    <span className="studyDescription text-justify">
                                    I completed my graduation from Computer Department in Daffodil International University. Having a lot of fun at Daffodil University
                                     </span>
                                 </Card.Text>
